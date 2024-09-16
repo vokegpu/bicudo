2080ded203f420c5db633bbc8fc291bc120c5cd4
MrsRina <diynggame@gmail.com>
2024-09-16 00:26:11 -0300

    [tweaks] HUD stuff

--- meow/src/main.cpp
+++ meow/src/main.cpp
@@ -48,30 +48,35 @@ void meow::render() {
     meow::app.immediate.draw(rect, color, p_objs->placement.angle);
     meow::app.rendering_placements_count++;
 
-    /*
-    meow::app.immediate.draw({p_objs->placement.min.x, p_objs->placement.min.y, 10.0f, 10.0f}, {0.0f, 1.0f, 1.0f, 1.0f}, 0.0f);
-    meow::app.immediate.draw({p_objs->placement.max.x, p_objs->placement.max.y, 10.0f, 10.0f}, {0.0f, 0.0f, 1.0f, 1.0f}, 0.0f);
+    if (meow::app.settings.show_vertices) {
+      meow::app.immediate.draw({p_objs->placement.vertices[0].x- camera.placement.pos.x, p_objs->placement.vertices[0].y - camera.placement.pos.y, 10.0f, 10.0f}, {1.0f, 0.0f, 1.0f, 1.0f}, 0.0f);
+      meow::app.immediate.draw({p_objs->placement.vertices[1].x- camera.placement.pos.x, p_objs->placement.vertices[1].y - camera.placement.pos.y, 10.0f, 10.0f}, {1.0f, 0.0f, 1.0f, 1.0f}, 0.0f);
+      meow::app.immediate.draw({p_objs->placement.vertices[2].x- camera.placement.pos.x, p_objs->placement.vertices[2].y - camera.placement.pos.y, 10.0f, 10.0f}, {1.0f, 0.0f, 1.0f, 1.0f}, 0.0f);
+      meow::app.immediate.draw({p_objs->placement.vertices[3].x- camera.placement.pos.x, p_objs->placement.vertices[3].y - camera.placement.pos.y, 10.0f, 10.0f}, {1.0f, 0.0f, 1.0f, 1.0f}, 0.0f);
+      meow::app.immediate.draw({rect.x - camera.placement.pos.x + p_objs->placement.size.x / 2, rect.y - camera.placement.pos.y + p_objs->placement.size.y / 2, 10.0f, 10.0f}, {1.0f, 0.0f, 1.0f, 1.0f}, 0.0f);
+    }
 
-    meow::app.immediate.draw({p_objs->placement.vertices[0].x, p_objs->placement.vertices[0].y, 10.0f, 10.0f}, {1.0f, 0.0f, 1.0f, 1.0f}, 0.0f);
-    meow::app.immediate.draw({p_objs->placement.vertices[1].x, p_objs->placement.vertices[1].y, 10.0f, 10.0f}, {1.0f, 0.0f, 1.0f, 1.0f}, 0.0f);
-    meow::app.immediate.draw({p_objs->placement.vertices[2].x, p_objs->placement.vertices[2].y, 10.0f, 10.0f}, {1.0f, 0.0f, 1.0f, 1.0f}, 0.0f);
-    meow::app.immediate.draw({p_objs->placement.vertices[3].x, p_objs->placement.vertices[3].y, 10.0f, 10.0f}, {1.0f, 0.0f, 1.0f, 1.0f}, 0.0f);
-    meow::app.immediate.draw({rect.x + p_objs->placement.size.x / 2, rect.y + p_objs->placement.size.y / 2, 10.0f, 10.0f}, {1.0f, 0.0f, 1.0f, 1.0f}, 0.0f);
-    */
+    if (meow::app.settings.show_aabb) {
+      meow::app.immediate.draw({p_objs->placement.min.x - camera.placement.pos.x, p_objs->placement.min.y - camera.placement.pos.y, 10.0f, 10.0f}, {0.0f, 1.0f, 1.0f, 1.0f}, 0.0f);
+      meow::app.immediate.draw({p_objs->placement.max.x - camera.placement.pos.x, p_objs->placement.max.y - camera.placement.pos.y, 10.0f, 10.0f}, {0.0f, 0.0f, 1.0f, 1.0f}, 0.0f);
+    }
   }
 
-  rect.z = 10.0f;
-  rect.w = 10.0f;
+  if (meow::app.settings.show_collision_info) {
+    rect.z = 10.0f;
+    rect.w = 10.0f;
 
-  rect.x = bicudo::app.world_manager.simulator.collision_info.start.x;
-  rect.y = bicudo::app.world_manager.simulator.collision_info.start.y;
+    rect.x = bicudo::app.world_manager.simulator.collision_info.start.x - camera.placement.pos.x;
+    rect.y = bicudo::app.world_manager.simulator.collision_info.start.y - camera.placement.pos.y;
 
-  meow::app.immediate.draw(rect, {1.0f, 0.0f, 0.0f, 1.0f}, 0.0f);
+    meow::app.immediate.draw(rect, {1.0f, 0.0f, 0.0f, 1.0f}, 0.0f);
 
-  rect.x = bicudo::app.world_manager.simulator.collision_info.end.x;
-  rect.y = bicudo::app.world_manager.simulator.collision_info.end.y;
+    rect.x = bicudo::app.world_manager.simulator.collision_info.end.x - camera.placement.pos.x;
+    rect.y = bicudo::app.world_manager.simulator.collision_info.end.y - camera.placement.pos.y;
 
-  meow::app.immediate.draw(rect, {0.0f, 1.0f, 0.0f, 1.0f}, 0.0f);
+    meow::app.immediate.draw(rect, {0.0f, 1.0f, 0.0f, 1.0f}, 0.0f);
+  }
+
   meow::app.immediate.revoke();
 }
 
@@ -84,7 +89,7 @@ int32_t main(int32_t, char**) {
 
   SDL_Window *p_sdl_win {
     SDL_CreateWindow(
-      "oiiii",
+      "meow 🐈",
       10,
       10,
       800,
@@ -171,7 +176,7 @@ int32_t main(int32_t, char**) {
     &pipeline_create_info
   );
 
-  ekg::frame("oiii muuu", {20, 20}, {400, 200})
+  ekg::frame("oiii muuu", {20, 20}, {400, 400})
     ->set_resize(ekg::dock::left | ekg::dock::bottom | ekg::dock::right | ekg::dock::top)
     ->set_drag(ekg::dock::full);
 
@@ -238,6 +243,16 @@ int32_t main(int32_t, char**) {
     ->range<uint64_t>(0, 0, 0, 10000)
     ->range<uint64_t>(0).u64.transfer_ownership(&meow::app.rendering_placements_count)
     ->set_text_align(ekg::dock::center | ekg::dock::left);
+
+  ekg::label("---", ekg::dock::next);
+  ekg::checkbox("Show AABB", false, ekg::dock::next | ekg::dock::fill)
+    ->transfer_ownership(&meow::app.settings.show_aabb);
+
+  ekg::checkbox("Show Collision Info", false, ekg::dock::next | ekg::dock::fill)
+    ->transfer_ownership(&meow::app.settings.show_collision_info);
+
+  ekg::checkbox("Show Vertices", false, ekg::dock::next | ekg::dock::fill)
+    ->transfer_ownership(&meow::app.settings.show_vertices);
 
   ekg::ui::label *p_position {ekg::label("", ekg::dock::next | ekg::dock::fill)};
 
--- meow/src/meow.hpp
+++ meow/src/meow.hpp
@@ -5,11 +5,19 @@
 #include "graphics/immediate.hpp"
 
 namespace meow {
+  struct settings {
+  public:
+    bool show_aabb {};
+    bool show_collision_info {};
+    bool show_vertices {};
+  };
+
   extern struct application {
   public:
     meow::immediate_graphics immediate {};
     meow::tools::pickup_info_t object_pickup_info {};
     meow::tools::pickup_info_t camera_pickup_info {};
+    meow::settings settings {};
     uint64_t rendering_placements_count {};
   } app;
 
